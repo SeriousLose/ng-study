59324d40b05ee015a62c6d2ac1d2cece59888ed5
helongfei <helongfei>
2020-07-15 17:00:12 +0800

    <feature> ng-content学习

---
+++ src/app/components/demo/demo.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ContentChild, TemplateRef } from '@angular/core';
+
+@Component({
+  selector: 'fl-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.less']
+})
+export class DemoComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
