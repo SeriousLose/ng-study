157b53ecc599c17230a075db0b8f16f1a624e715
SeriousLose <643756132@qq.com>
2021-03-15 00:08:45 +0800

    feature
    样式文件

--- src/app/components/other-base/other-base.component.ts
+++ src/app/components/other-base/other-base.component.ts
@@ -5,6 +5,8 @@ import { BaseComponent } from '../base/base.component';
   selector: 'l-other-base',
   templateUrl: './other-base.component.html',
   styleUrls: ['./other-base.component.less']
+  // styleUrls: BaseComponent.styleUrls.concat(["./other.component.less"])
+  // styleUrls: ['./base/base.component.css', "./other.component.less"]
 })
 export class OtherBaseComponent extends BaseComponent {
   aa = 'bb';
