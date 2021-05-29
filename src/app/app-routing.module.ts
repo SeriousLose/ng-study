5c215f552e5f951602cdc43827f61880486f5055
helongfei <643756132@qq.com>
2021-05-29 20:24:59 +0800

    <feature> 懒加载

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-
-const routes: Routes = [{ path: 'feature3', loadChildren: () => import('./feature3/feature3.module').then(m => m.Feature3Module) }];
+const routes: Routes = [
+  {
+    path: 'feature3',
+    // loadChildren: () => import('./feature3/feature3.module').then((m) => m.Feature3Module),
+    loadChildren: './feature3/feature3.module#Feature3Module'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
