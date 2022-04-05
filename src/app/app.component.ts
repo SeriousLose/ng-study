8228e9320f6ba119eaf35f46576be7855d1a0fac
helongfei <643756132@qq.com>
2022-04-05 15:05:20 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
+export class AppComponent  implements  AfterViewInit{
   title = 'ng-study';
+  constructor(private elementRef: ElementRef) {}
+
+  // 模板中的元素已创建完成
+  ngAfterViewInit() {
+    console.dir(this.elementRef.nativeElement.querySelector('div'));
+  }
 }
